66c66a453912e53d3bac9a7cb939a4f15b372f90
Blackbox19 <trevorirwin819@gmail.com>
2024-04-08 14:31:17 -0400

    finished

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const printStudents = require('./lib/printStudents.js')
-const addStudent = require('./lib/addStudent.js')
+const {print, add} = require('./lib/studentTools.js')
+// The array above = the two constants below
+// const printStudents = studentTools[0]
+// const addStudent = studentTools[1]
+
+const dayjs = require('dayjs')
+
 const studentName = process.argv[2]
 
 if (studentName === 'print') {
-    printStudents()
+    print()
     
 } else {
-    addStudent(studentName)
+    const date = dayjs().format('M/D/YYYY')
+    add(studentName + '-' + date)
 }
