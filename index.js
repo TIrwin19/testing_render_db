321093abd726988f0baf25130ee1dafcca68162c
Blackbox19 <trevorirwin819@gmail.com>
2024-04-08 11:51:57 -0400

    finished basic modularization

--- index.js
+++ index.js
@@ -1,71 +1,10 @@
-const args = process.argv
-// console.log('browser stuff', document, window)
-// Get two number arguments from the user's command list (process.argv)
+const printStudents = require('./lib/printStudents.js')
+const addStudent = require('./lib/addStudent.js')
+const studentName = process.argv[2]
 
-// const num1 = args[2]
-// const num2 = args[3]
-
-// Add those two numbers together
-// console.log(Number(num1) + Number(num2))
-// console.log('The sum is:', +num1 + +num2)
-
-// console.log the sum of the two numbers
-
-
-
-// these are the same
-// function printSomething(str) {
-//     console.log(str)
-// }
-// these are the same
-// printSomething('some string')
-
-// const sum = (num1, num2) => num1 + num2
-
-// const add = sum(5, 20)
-
-// console.log(add)
-
-
-// a wrest or '...' makes an array, mist be in a function as an argument
-// function printStudents(...arr){
-//     console.log(arr)
-// }
-
-// printStudents('Wes', 'Will', 'Musin', 'Vince', 'juan', 'Alice')
-
-
-
-// const fruits = ['orange', 'apple']
-
-// const copy = ['kiwi', ...fruits, 'banana']
-
-// console.log(copy)
-
-// const data = {
-//     name: JD,
-//     age: 44
-// }
-
-// const clone = {...data}
-
-// console.log(clone)
-
-
-
-// const data = {
-//     name: JD,
-//     age: 44
-// }
-
-// const {name, age, blah} = data
-
-// console.log(data)
-
-
-
-function someFunc({age}, str, num){
-    console.log(num)
+if (studentName === 'print') {
+    printStudents()
+    
+} else {
+    addStudent(studentName)
 }
-
-someFunc({name: 'bob', age: 99}, 'another', 15)
