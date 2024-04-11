687332449ce82c09460bb46ffc4f65f6dc3d4885
Blackbox19 <trevorirwin819@gmail.com>
2024-04-11 10:10:13 -0400

    done oop

--- index.js
+++ index.js
@@ -1,35 +1,89 @@
-class Person {
-    constructor(name, age, hobbies) {
-        this.name = name
-        this.age = age
-        this.hobbies = hobbies
+// class Person {
+//     species = 'Homosapien'
+
+//     constructor(name, age, hobbies) {
+//         this.name = name
+//         this.age = age
+//         this.hobbies = hobbies
+//     }
+
+//     haveBirthday() {
+//         this.age++
+//         console.log('Happy Birthday', this.name)
+//     }
+
+//     printBirthday() {
+//         console.log(`${this.name} is ${this.age} years old.`)
+//     }
+
+//     printHobbies() {
+//         console.log('\nHobbies\n----------')
+
+//         this.hobbies.forEach((hobby) => {
+//             console.log(hobby)
+//         })
+//     }
+// }
+
+// const jd = new Person('JD', 44, ['fishing', 'pickleball'])
+
+// console.log(jd)
+// console.log(jd.species)
+
+// jd.haveBirthday()
+// jd.printBirthday()
+// jd.printHobbies()
+
+
+class Phone {
+    screen = true
+
+    constructor(number, size, model, color) {
+        this.number = number
+        this.size = size
+        this.model = model
+        this.color = color
     }
 
-    haveBirthday() {
-        this.age++
-        console.log('Happy Birthday', this.name)
-    }
-
-    printBirthday(){
-        console.log(`${this.name} is ${this.age} years old.`)
-    }
-
-    printHobbies(){
-        console.log('\nHobbies\n----------')
-
-        this.hobbies.forEach((hobby) => {
-            console.log(hobby)
-        })
+    printModel(){
+        console.log('Model:', 'Base')
     }
 }
 
-const jd = new Person('JD', 44, ['fishing', 'pickleball'])
+class iphone extends Phone {
+    faceTime = true
 
-console.log(jd)
+    constructor(number, size, model, color, appleID){
+        super(number, size, model, color)
+        this.appleID = appleID
+    }
 
-jd.haveBirthday()
-jd.printBirthday()
-jd.printHobbies()
+    printModel(){
+        console.log('iPhone:', this.model)
+    }
+}
+
+class samsung extends Phone {
+    foldable = true
+
+    constructor(number, size, model, color, repairID){
+        super(number, size, model, color)
+        this.repairID = repairID
+    }
+
+}
+
+const jdPhone = new iphone('777-777-7777', 'standard', '15', 'gray', 'ygliugliug')
+const bobPhone = new iphone('777-777-7777', 'standard', '12', 'green', 'iaedportgp')
+const sarahPhone = new samsung('777-777-7777', 'foldable', '23', 'blue', '123456789')
+
+console.log(jdPhone)
+console.log(bobPhone)
+console.log(sarahPhone)
+
+jd.printModel()
+
+
 
 
 // function Person(name, age, hobbies) {
