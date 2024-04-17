85036f1b856ef396b6095a9e79aa9207e46561ab
Blackbox19 <trevorirwin819@gmail.com>
2024-04-16 20:27:26 -0400

    for Muhsin

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,9 +1,17 @@
 const router = require('express').Router()
 const { v4: generateID } = require('uuid')
-const data = require('../db/data')
+// const data = require('../db/users.json')
+const fs = require('fs/promises')
+
+async function getData(){
+    const data = await fs.readFile('./db/users.json', 'utf8')
+
+    return JSON.parse(data)
+}
 
 // Get all users
-router.get('/users', (req, res) => {
+router.get('/users', async (req, res) => {
+    const data = await getData()
     const nameQuery = req.query.name?.toLowerCase()
 
     if (nameQuery) {
@@ -16,7 +24,8 @@ router.get('/users', (req, res) => {
 })
 
 // Get user by ID
-router.get('/users/:id', (req, res) => {
+router.get('/users/:id', async (req, res) => {
+    const data = await getData()
     const paramId = req.params.id
 
     const user = data.find(uObj => uObj.id == paramId)
@@ -30,16 +39,18 @@ router.post('/users/form', (req, res) => {
     res.redirect('/')
 })
 
-router.post('/users', (req, res) => {
+router.post('/users', async (req, res) => {
     const id = generateID()
+    const data = await getData()
 
     data.push({
         ...req.body,
         id: id
     })
 
+    await fs.writeFile('./db/users.json', JSON.stringify(data, null, 2))
+
     res.json({ message: 'User has been added!' })
 })
-
 
 module.exports = router
